35f219ae51db885081d5cb8a37e3564c00c41fe1
Payal429 <payal.sunil429@gmail.com>
2025-08-04 12:57:46 +0200

    Added the pages, header and footer

--- Lungisa/Controllers/HomeController.cs
+++ Lungisa/Controllers/HomeController.cs
@@ -9,7 +9,34 @@ namespace Lungisa.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        // Loads the Homepage view
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Loads the About Us page
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        // Loads the Contact Us page
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        // Loads the Latest News page
+        public ActionResult News()
+        {
+            return View();
+        }
+        public ActionResult Services()
+        {
+            return View();
+        }
+        public ActionResult Donations()
         {
             return View();
         }
